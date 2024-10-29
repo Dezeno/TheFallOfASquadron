482465a9bcd9e875a567ee0e28392033315de33a
Dezeno <enzo.duquesne@outlook.fr>
2024-10-29 19:29:52 +0100

    Added Assets to the project

--- TheFallOfASquadron/Assets/Scripts/HealMovement.cs
+++ TheFallOfASquadron/Assets/Scripts/HealMovement.cs
@@ -9,7 +9,7 @@ public class HealMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,7 +17,7 @@ public class HealMovement : MonoBehaviour
     {
         transform.Translate(Vector3.forward * Time.deltaTime * -speed);
 
-        if(transform.position.z < -10)
+        if (transform.position.z < -10)
         {
             Destroy(gameObject);
         }
